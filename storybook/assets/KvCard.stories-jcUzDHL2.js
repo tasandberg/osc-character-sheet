718cbad4ad6986ea3673a29c659db8acc958d48f
tasandberg <tasandberg@users.noreply.github.com>
2026-07-03 17:30:08 +0000

    deploy: 6bae424d026131d4a7498c9087a82720e3c03d94

---
+++ storybook/assets/KvCard.stories-jcUzDHL2.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./iframe-CmEGzJ4q.js";import{n,r}from"./Card-BxIYo_Z6.js";import{n as i,t as a}from"./Stamp-CGXBp1U4.js";var o,s,c,l;e((()=>{r(),i(),o=t(),s={title:`Layout / KvCard`},c=()=>(0,o.jsxs)(n,{children:[(0,o.jsx)(`div`,{className:`head`,children:(0,o.jsx)(a,{children:`STR`})}),(0,o.jsx)(`div`,{className:`val`,children:`16`})]}),c.__docgenInfo={description:``,methods:[],displayName:`KeyValue`},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`() => <KvCard>
+    <div className="head">
+      <Stamp>STR</Stamp>
+    </div>
+    <div className="val">16</div>
+  </KvCard>`,...c.parameters?.docs?.source}}},l=[`KeyValue`]}))();export{c as KeyValue,l as __namedExportsOrder,s as default};
