c6819692eb9ba3b986414bf6b95dde520d67669b
tasandberg <tasandberg@users.noreply.github.com>
2026-07-08 04:51:02 +0000

    deploy: 95052e7a20115b9637ff6b68e3fdb3d0cd1ba459

---
+++ storybook/assets/Tag.stories-Bn5wAREf.js
@@ -0,0 +1,18 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./iframe-CYViEK4o.js";import{n,t as r}from"./Tag-DWEZ_4HZ.js";var i,a,o,s,c,l;e((()=>{n(),i=t(),a={title:`Display / Tag`},o=()=>(0,i.jsxs)(`div`,{className:`u-row u-gap-3 u-wrap`,children:[(0,i.jsx)(r,{children:`Neutral`}),(0,i.jsx)(r,{intent:`teal`,children:`Teal`}),(0,i.jsx)(r,{intent:`crimson`,children:`Crimson`}),(0,i.jsx)(r,{intent:`forest`,children:`Forest`}),(0,i.jsx)(r,{intent:`mustard`,children:`Mustard`}),(0,i.jsx)(r,{intent:`solid`,children:`Solid`})]}),s=()=>(0,i.jsxs)(`div`,{className:`u-row u-gap-3 u-wrap`,children:[(0,i.jsx)(r,{onRemove:()=>{},removeLabel:`Remove Common`,children:`Common`}),(0,i.jsx)(r,{onRemove:()=>{},removeLabel:`Remove Elvish`,children:`Elvish`})]}),c=()=>(0,i.jsxs)(`div`,{className:`u-row u-gap-2 u-wrap`,children:[(0,i.jsx)(r,{variant:`chip`,className:`melee`,icon:`fa-sword`,tooltip:`Melee`,title:`Melee`}),(0,i.jsx)(r,{variant:`chip`,className:`missile`,icon:`fa-bow-arrow`,tooltip:`Missile`,title:`Missile`}),(0,i.jsx)(r,{variant:`chip`,icon:`fa-hand-fist`,tooltip:`Blunt`,title:`Blunt`}),(0,i.jsx)(r,{variant:`chip`,title:`Two-handed`,children:(0,i.jsx)(`span`,{className:`tag-txt`,children:`2H`})})]}),o.__docgenInfo={description:``,methods:[],displayName:`Intents`},s.__docgenInfo={description:``,methods:[],displayName:`Removable`},c.__docgenInfo={description:``,methods:[],displayName:`Chips`},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => <div className="u-row u-gap-3 u-wrap">
+    <Tag>Neutral</Tag>
+    <Tag intent="teal">Teal</Tag>
+    <Tag intent="crimson">Crimson</Tag>
+    <Tag intent="forest">Forest</Tag>
+    <Tag intent="mustard">Mustard</Tag>
+    <Tag intent="solid">Solid</Tag>
+  </div>`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`() => <div className="u-row u-gap-3 u-wrap">
+    <Tag onRemove={() => {}} removeLabel="Remove Common">Common</Tag>
+    <Tag onRemove={() => {}} removeLabel="Remove Elvish">Elvish</Tag>
+  </div>`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`() => <div className="u-row u-gap-2 u-wrap">
+    <Tag variant="chip" className="melee" icon="fa-sword" tooltip="Melee" title="Melee" />
+    <Tag variant="chip" className="missile" icon="fa-bow-arrow" tooltip="Missile" title="Missile" />
+    <Tag variant="chip" icon="fa-hand-fist" tooltip="Blunt" title="Blunt" />
+    <Tag variant="chip" title="Two-handed">
+      <span className="tag-txt">2H</span>
+    </Tag>
+  </div>`,...c.parameters?.docs?.source}}},l=[`Intents`,`Removable`,`Chips`]}))();export{c as Chips,o as Intents,s as Removable,l as __namedExportsOrder,a as default};
