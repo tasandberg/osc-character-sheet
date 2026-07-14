92c19fd849deb9d04edac63da52cf1abb7371dba
tasandberg <tasandberg@users.noreply.github.com>
2026-07-14 05:42:22 +0000

    deploy: 623108450bd17dd24b5a6494c0d6eee45969a716

---
+++ storybook/assets/Skeleton.stories-DPnqAgxT.js
@@ -0,0 +1,10 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./iframe-DmyqL6ir.js";import{n,t as r}from"./Skeleton-CSOCnHQ2.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`Overlays / Skeleton`},o=()=>(0,i.jsxs)(`div`,{style:{display:`flex`,flexDirection:`column`,gap:10},children:[(0,i.jsx)(r,{width:`60%`,height:20}),(0,i.jsx)(r,{width:`90%`,height:14}),(0,i.jsx)(r,{width:`75%`,height:14}),(0,i.jsx)(r,{width:`40%`,height:12})]}),o.__docgenInfo={description:``,methods:[],displayName:`Bars`},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => <div style={{
+  display: "flex",
+  flexDirection: "column",
+  gap: 10
+}}>
+    <Skeleton width="60%" height={20} />
+    <Skeleton width="90%" height={14} />
+    <Skeleton width="75%" height={14} />
+    <Skeleton width="40%" height={12} />
+  </div>`,...o.parameters?.docs?.source}}},s=[`Bars`]}))();export{o as Bars,s as __namedExportsOrder,a as default};
