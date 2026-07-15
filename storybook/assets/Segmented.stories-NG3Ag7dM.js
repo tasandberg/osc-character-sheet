e2f372d870a96f4563f3bb714432d016d5bc4f69
tasandberg <tasandberg@users.noreply.github.com>
2026-07-15 04:35:27 +0000

    deploy: c62fa29904171eda9d8ee0e32f9a38c13f06ea06

---
+++ storybook/assets/Segmented.stories-NG3Ag7dM.js
@@ -0,0 +1,10 @@
+import{i as e,s as t}from"./preload-helper-xPQekRTU.js";import{n,t as r}from"./iframe-xPPBcjwE.js";import{n as i,t as a}from"./Segmented-C6o2XqAx.js";var o,s,c,l,u;e((()=>{o=t(n(),1),i(),s=r(),c={title:`Controls / Segmented`},l=()=>{let[e,t]=o.useState(`combat`);return(0,s.jsx)(a,{options:[{value:`combat`,label:`Combat`},{value:`spells`,label:`Spells`}],value:e,onValueChange:t})},l.__docgenInfo={description:``,methods:[],displayName:`Interactive`},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`() => {
+  const [value, setValue] = React.useState("combat");
+  return <Segmented options={[{
+    value: "combat",
+    label: "Combat"
+  }, {
+    value: "spells",
+    label: "Spells"
+  }]} value={value} onValueChange={setValue} />;
+}`,...l.parameters?.docs?.source}}},u=[`Interactive`]}))();export{l as Interactive,u as __namedExportsOrder,c as default};
